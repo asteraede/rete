474539cf19359a1ffea9a7c12915b303658438a0
asteraede <asteraede@gmail.com>
2022-10-15 05:19:06 -0400

    updated source.

--- source/xos/app/console/rete/version/main_opt.hpp
+++ source/xos/app/console/rete/version/main_opt.hpp
@@ -16,7 +16,7 @@
 ///   File: main_opt.hpp
 ///
 /// Author: $author$
-///   Date: 5/8/2022
+///   Date: 5/8/2022, 10/15/2022
 ///////////////////////////////////////////////////////////////////////
 #ifndef XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPT_HPP
 #define XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPT_HPP
@@ -28,6 +28,24 @@
 #define XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPTIONS_CHARS_EXTEND \
 
 #define XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPTIONS_OPTIONS_EXTEND \
+
+///////////////////////////////////////////////////////////////////////
+#define XOS_APP_CONSOLE_RETE_VERSION_MAIN_HELP_OPTIONS_CHARS \
+   XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPTIONS_CHARS_EXTEND \
+   XOS_APP_CONSOLE_VERSION_MAIN_HELP_OPTIONS_CHARS
+
+#define XOS_APP_CONSOLE_RETE_VERSION_MAIN_HELP_OPTIONS_OPTIONS \
+   XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPTIONS_OPTIONS_EXTEND \
+   XOS_APP_CONSOLE_VERSION_MAIN_HELP_OPTIONS_OPTIONS
+
+///////////////////////////////////////////////////////////////////////
+#define XOS_APP_CONSOLE_RETE_VERSION_MAIN_USAGE_OPTIONS_CHARS \
+   XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPTIONS_CHARS_EXTEND \
+   XOS_APP_CONSOLE_VERSION_MAIN_USAGE_OPTIONS_CHARS
+
+#define XOS_APP_CONSOLE_RETE_VERSION_MAIN_USAGE_OPTIONS_OPTIONS \
+   XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPTIONS_OPTIONS_EXTEND \
+   XOS_APP_CONSOLE_VERSION_MAIN_USAGE_OPTIONS_OPTIONS
 
 ///////////////////////////////////////////////////////////////////////
 #define XOS_APP_CONSOLE_RETE_VERSION_MAIN_LOGGING_OPTIONS_CHARS \
@@ -103,6 +121,45 @@ protected:
             }
         }
         return err;
+    }
+
+    /// ...option...
+    virtual int on_option
+    (int optval, const char_t* optarg, const char_t* optname,
+     int optind, int argc, char_t**argv, char_t**env) {
+        int err = 0;
+        switch(optval) {
+        default:
+            err = extends::on_option(optval, optarg, optname, optind, argc, argv, env);
+        }
+        return err;
+    }
+    virtual const char_t* option_usage(const char_t*& optarg, const struct option* longopt) {
+        const char_t* chars = "";
+        switch(longopt->val) {
+        default:
+            chars = extends::option_usage(optarg, longopt);
+            break;
+        }
+        return chars;
+    }
+    virtual const char_t* options(const struct option*& longopts) {
+        static const char_t* chars = XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPTIONS_CHARS;
+        static struct option optstruct[]= {
+            XOS_APP_CONSOLE_RETE_VERSION_MAIN_OPTIONS_OPTIONS
+            {0, 0, 0, 0}};
+        longopts = optstruct;
+        return chars;
+    }
+
+    /// ...argument...
+    virtual const char_t* arguments(const char_t**& argv) {
+        static const char_t* _args = XOS_APP_CONSOLE_RETE_VERSION_MAIN_ARGS;
+        static const char_t* _argv[] = {
+            XOS_APP_CONSOLE_RETE_VERSION_MAIN_ARGV
+            0};
+        argv = _argv;
+        return _args;
     }
 
 protected:
